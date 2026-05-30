3b7ed9631c597f626ca6ee201360f6a6e5ec52b2
AliJahani33 <alijahani1919@gmail.com>
2026-05-30 17:04:19 +0330

    Final Deploy - Web3Forms and GitHub Actions added

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  // basePath: '/AliJahai_Site', // Uncomment this if deploying to a project repository instead of user root
+  basePath: '/AliJahai_Site', // Required if deploying to https://alijahani33.github.io/AliJahai_Site
   images: { unoptimized: true }, // Required for static export with next/image, even if not strictly used now
 };
 
